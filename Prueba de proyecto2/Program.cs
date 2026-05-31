8e57a14bbedbff9d2b40f1825dc91b9384492a39
moralesjk88-droid <moralesjk88@gmail.com>
2026-05-30 21:32:23 -0600

    Agregar archivos de proyecto.

---
+++ Prueba de proyecto2/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+public class Dueno
+{
+    private int idDueño;
+    private string nombre;
+    private string dpi;
+    private string nit;
+
+    public int IdDueño
+    {
+        get
+        {
+            return idDueño;
+        }
+        set
+        {
+           idDueño= value;
+        }
+    }
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.Length >=3)
+                nombre= value;
+            else
+            {
+                Console.WriteLine("Nombre invalido (minimo 3 caracteres");
+                Environment.Exit(0);
+         
+            }
+        }
+    }
+
+    public string Dpi
+    {
+        get { return dpi; }
+        set {
+            if ( value.Length==13 && long.TryParse (value, out _))
+            dpi = value;
+            else
+            {
+                Console.WriteLine("No puede tener menos de 13 caracteres");
+                Environment.Exit (0);
+            }
+        }
+    }
+
+    public string Nit
+    {
+        get { return nit; }
+        set
+        {
+            if (value.Length == 9 && long.TryParse(value, out _))
+                nit = value;
+            else
+            {
+                Console.WriteLine("No puede tener menos de 9 caracteres");
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    public Dueno (string dpi,string nombre, string nit)
+    {
+        
+        Nombre= nombre;
+        Dpi= dpi;
+        Nit= nit;
+    }
+    }
+
+public class Vehiculo
+{
+    private int idCarro;
+    private string tipo;
+    private string marca;
+    private string linea; 
+    private string noVin;
+    private double costoInicial;
+    private string fechaIngreso;
+    private string estado;
+    private int idDueño;
+
+    public int IdCarro
+    {
+        get
+        {
+            return idCarro;
+        }
+        set { idCarro = value; }
+            }
+
+    public string Tipo
+    {
+        get
+        {
+            return tipo;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.Length<3 )
+            {
+                tipo = value;
+            }
+            else
+            {
+                Console.WriteLine("Ingreso invalido(Minimo 3 caracteres)");
+                Environment.Exit(0);
+            
+            }
+        }
+    }
+
+    public string Marca
+    {
+        get
+        {
+            return marca;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.Length < 3)
+            {
+                marca = value;
+            }
+            else
+            {
+                Console.WriteLine("Ingreso invalido(Minimo 3 caracteres)");
+                Environment.Exit(0);
+
+            }
+        }
+    }  
+    public string Linea
+    {
+        get
+        {
+            return linea;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.Length < 3)
+            {
+                linea = value;
+            }
+            else
+            {
+                Console.WriteLine("Ingreso invalido(Minimo 3 caracteres)");
+                Environment.Exit(0);
+
+            }
+        }
+    }
+    public string NoVin
+    {
+        get { return noVin; }
+        set
+        {
+            if (value.Length == 17 && long.TryParse(value, out _))
+                noVin = value;
+            else
+            {
+                Console.WriteLine("No puede tener menos de 17 caracteres");
+                Environment.Exit(0);
+            }
+        }
+    }
+
+   public double CostoInicial
+    {
+        get
+        { return costoInicial; }
+        set
+        {
+            if (value >= 0)
+            {
+                costoInicial = value;
+            }
+            else
+            {
+                Console.WriteLine("El costo no puedser negativo");
+                Environment.Exit(0);
+            }
+        }
+
+    }
+    public string FechaIngreso
+    {
+        get { return fechaIngreso; }
+    
+        set { fechaIngreso = value; }
+    }
+    public string Estado
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(estado))
+            {
+                return "No definido";
+
+            }
+            return estado;
+        }
+
+        set
+        {
+            if (value== "activo"|| value=="en reparacion"|| value=="finalizado" || value=="entregado")
+            {
+              estado = value;      
+            }
+        }
+
+    }
+
+    public int IdDueño
+    {
+        get
+        { return idDueño; }
+        set
+        {
+            idDueño = value;
+        }
+    }
+
+    public Vehiculo(string tipo, string marca, string linea, string noVin, double costoInicial, string fechaIngreso, string estado, int idDueño)
+    {
+        Tipo=tipo;
+        Marca=marca;
+        NoVin=noVin;
+        Linea=linea;
+        CostoInicial=costoInicial;
+        FechaIngreso=fechaIngreso;
+        Estado=estado;
+        IdDueño = idDueño;
+    }
+    }
+
+public class GastoFinanciero
+{
+    private int idGasto;
+    private double monto;
+
+    public int IdGasto
+    {
+        get { return idGasto; }
+        set { idGasto = value; }
+    }
+    public double Monto
+    {
+        get { return monto; } 
+        set
+        {
+            if (value >= 0)
+            {
+                monto = value;
+            }
+            else
+            {
+                Console.WriteLine("El monto no puedser negativo");
+                Environment.Exit(0);
+            }
+        }
+    }
+    public GastoFinanciero(double monto)
+    {
+        monto = monto;
+    }
+}
+
+public class Reparacion:GastoFinanciero
+{
+    private string tipoReparacion;
+    private string cantidadReparacion;
+    private string cantidadpiezas;
+    private int idCarro;
+
+    public string TipoReparacion
+    {
+        get { if (string.IsNullOrWhiteSpace(tipoReparacion))
+            {
+                return "No especificada";
+            }
+            return tipoReparacion;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value)&& value.Length<3)
+            {
+                tipoReparacion= value;
+                
+            }
+            else
+            {
+                Console.WriteLine("Ingreso invalido(Minimo 3 caracteres)");
+                Environment.Exit(0);
+
+            }
+
+        }
+    }
+
+    public string CantidadReparacion
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(cantidadReparacion))
+            {
+                return "0";
+            }
+            return cantidadReparacion;
+        }
+        set
+        {
+            if (int.TryParse(value, out int cantidad) && cantidad>=0)
+            {
+                cantidadReparacion = value;
+            }
+            else
+            {
+                Console.WriteLine("La cantidad no puede ser negativa");
+            }
+        }
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
